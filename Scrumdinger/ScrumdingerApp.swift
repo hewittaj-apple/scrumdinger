42c555cab22898bcd763bd923c02d8c577d3d6d1
Alex Hewitt <hewittaj@outlook.com>
2023-03-13 20:32:23 -0500

    initial commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Alex Hewitt on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
