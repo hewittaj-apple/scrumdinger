96124ab77daf039e9d6c33bdc6ab6f5e95cc560a
Alex Hewitt <hewittaj@outlook.com>
2023-03-15 16:51:41 -0500

    add card view and scrum view skeleton

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,20 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Alex Hewitt on 3/15/23.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView()
+    }
+}
